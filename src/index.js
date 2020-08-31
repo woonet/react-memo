d139ac696e9f3c61d3938937a295b1f97304c268
woonet <woo9347@gmail.com>
2020-08-31 23:57:13 +0900

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+import "./index.css";
+import App from "./App";
+import MemoStore, { memoReducer } from "./memo/Store";
 
+// Redux Persistの設定
+const persistConfig = {
+  key: "memo",
+  storage: storage,
+  blacklist: ["message", "mode", "fdata"],
+  whitelist: ["data"],
+};
+
+// パーシストレデューサーの作成
+const persistedReducer = persistReducer(persistConfig, memoReducer);
+
+// ストア、パーシスターの作成
+let store = createStore(persistedReducer);
+let pstore = persistStore(store);
+
+// 表示をレンダリング
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <PersistGate loading={<p>loading...</p>} persistor={pstore}>
+      <App />
+    </PersistGate>
+  </Provider>,
+  document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export default pstore;
